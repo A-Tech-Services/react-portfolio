5daa3b38060102a380fc8a3d928b14dfb36d893f
A-Tech-Services <abiodunoluwapelumi859@gmail.com>
2023-07-08 02:07:07 +0100

    update

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -1,13 +1,13 @@
 import React from "react";
 import Navbar from "../components/Navbar";
-import ProjectHeroImg  from "../components/ProjectHeroImg";
+import ProjectHeroImg  from "../components/HeroImg2";
 import Footer from "../components/Footer";
 
 const Project = () => {
     return(
         <div>
             <Navbar />
-            <ProjectHeroImg />
+            <HeroImg2 heading={"Projects"} paragraph={"This place contain heading paragraph."}/>
             <Footer />
         </div>
     )
