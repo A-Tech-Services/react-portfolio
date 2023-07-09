acc2128c3850efd3ee3d68d33dd385e26585f4e4
A-Tech-Services <abiodunoluwapelumi859@gmail.com>
2023-07-09 15:39:20 +0100

    new

---
+++ src/components/AboutContent.js
@@ -0,0 +1,34 @@
+import { Link } from "react-router-dom";
+import "./AboutStyles.css";
+import React from 'react';
+import React1 from "../images/react1.jpg";
+import React2 from "../images/react2.webp";
+
+const AboutContent = () => {
+  return (
+    <div className="about">
+        <div className="left">
+            <h1>Who am I?</h1>
+            <p>I'm a react front-end developer. I create 
+                responsive secure websites and digital products for my clients.
+            </p>
+
+            <Link to="/contact"><button className="btn">Contact</button></Link>
+        </div>
+
+        <div className="right">
+            <div className="img-container">
+                <div className="img-stack top">
+                    <img src={React1} className="img" alt="true"/>
+                </div>
+
+                <div className="img-stack bottom">
+                    <img src={React2} className="img" alt="true"/>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default AboutContent 
