375aaad9a0baefbf9862597576b011a6d2482528
A-Tech-Services <abiodunoluwapelumi859@gmail.com>
2023-07-09 16:03:01 +0100

    new

---
+++ src/components/Form.js
@@ -0,0 +1,26 @@
+import "./FormStyles.css";
+import React from "react";
+
+const Form = () =>{
+    return(
+        <div className="form">
+            <form>
+                <label>Your Name:</label>
+                <input type="text"></input>
+
+                <label>Email:</label>
+                <input type="text"></input>
+
+                <label>Subject:</label>
+                <input type="text"></input>
+
+                <label>Your Name:</label>
+                <textarea rows={6} placeholder="Type Your Message Here"/>
+
+                <button className="btn">Submit</button>
+            </form>
+        </div>
+    )
+}
+
+export default Form;
